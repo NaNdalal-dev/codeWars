cb968849e58f2b0f1c37496da6346f06ba4e2a99
NaNdalal-dev <dnandalal-dev>
2021-10-16 15:43:07 +0530

    Commit 12

---
+++ nonConsecutive.py
@@ -0,0 +1,6 @@
+#Link:
+#https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/train/python
+def first_non_consecutive(arr):
+	for i in range(1,len(arr)):
+		if abs(arr[i-1] - arr[i]) != 1:
+			return arr[i]
