dc27cfe76cc7e87506fc36339e9e38f27dce72de
NaNdalal-dev <dnandalal-dev>
2021-10-19 14:18:27 +0530

    Commit 18-2

--- num_to_str.py
+++ num_to_str.py
@@ -1,2 +1,1 @@
-numbers_list =['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
-switch_it_up = lambda index:numbers_list[index]
+switch_it_up = lambda index:['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine'][index]
