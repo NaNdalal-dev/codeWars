b928783508dbd1d19b705e2d73551577570e4ab6
NaNdalal-dev <dnandalal-dev>
2021-10-19 14:07:08 +0530

    Commit 16-2

--- MinSteps.py
+++ MinSteps.py
@@ -13,8 +13,3 @@ def minimum_steps(numbers, value):
     		return count+1
     	
     	count += 1
-print(minimum_steps([4,6,3], 7))
-print(minimum_steps([10,9,9,8], 17))
-print(minimum_steps([8,9,10,4,2], 23))
-print(minimum_steps([19,98,69,28,75,45,17,98,67], 464))
-print(minimum_steps([4,6,3], 2))
